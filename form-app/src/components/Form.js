5301031a868161b8f2b9e9caf027be83299e322d
bsihame <sihamebazi@pursuit.org>
2020-04-04 09:38:04 -0400

    add submit button amd Checked box

--- form-app/src/components/Form.js
+++ form-app/src/components/Form.js
@@ -3,29 +3,53 @@ import React from "react";
 class Form extends React.Component {
   state = {
     firstName: "",
-    lastName : ""
+    lastName: "",
+    agree:false,
   }
   
   
-  handleFirstNameChange = (e) =>{
-    this.setState({firstName: e.target.value})
-  }
+  handleFirstNameChange = (e) => {
+    this.setState({ firstName: e.target.value })
+  };
 
   handleLastNameChange = (e) => {
-    this.setState({lastName : e.target.value})
+    this.setState({ lastName: e.target.value })
+  };
+  handleAgree = () => {
+    
+    this.setState((prevState) => {
+      return {
+        agree: !prevState.agree
+      }
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      firsName: this.state.firstName,
+      lastName: this.state.lastName,
+      
+
+    })
   }
+  
   render() {
-    console.log(this.state.firstName)
-    console.log(this.state.lastName)
+    console.log(this.state)
 
 		return (
-			<form>
+			<form onSubmit={this.handleSubmit}>
 				<label>
           First Name:
 					<input type="text" placeholder="Enter Your Firs Name"value={this.state.firsName} onChange={this.handleFirstNameChange} />
           Last Name: 
           <input text="text" placeholder="Enter Your Last Name" value={this.state.lastName} onChange={this.handleLastNameChange}/>
-				</label>
+        </label>
+        <label>
+          Agree?
+           <input type="checkbox" checked={this.state.agree} onChange={this.handleAgree} />
+        </label>
+        <button type="submit">Submit</button>
 			</form>
 		);
 	}
