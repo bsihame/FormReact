5e5a3678d536205b84e02ecc2111d6b17c7b91d1
bsihame <sihamebazi@pursuit.org>
2020-04-04 09:02:28 -0400

    add  last name state

--- form-app/src/components/Form.js
+++ form-app/src/components/Form.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 class Form extends React.Component {
-	render() {
+  state = {
+    firstName: "",
+    lastName : ""
+  }
+  
+  
+  handleFirstNameChange = (e) =>{
+    this.setState({firstName: e.target.value})
+  }
+
+  handleLastNameChange = (e) => {
+    this.setState({lastName : e.target.value})
+  }
+  render() {
+    console.log(this.state.firstName)
+    console.log(this.state.lastName)
+
 		return (
 			<form>
-        <label>
-          First Name: 
-					<input type="text" value="name" onChange={handleFirstNameChange} />
+				<label>
+          First Name:
+					<input type="text" placeholder="Enter Your Firs Name"value={this.state.firsName} onChange={this.handleFirstNameChange} />
+          Last Name: 
+          <input text="text" placeholder="Enter Your Last Name" value={this.state.lastName} onChange={this.handleLastNameChange}/>
 				</label>
 			</form>
 		);
